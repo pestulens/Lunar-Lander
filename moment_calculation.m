71b1382f6fb585ebe5c83ad762720aa041c6e3ba
鄭比綸的實驗室筆電 <s39283132@gmail.com>
2024-04-28 21:48:46 +0800

    update

--- moment_calculation.m
+++ moment_calculation.m
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-close all;clc;
-r = 0.3; %radius(m)
-a = 0:45:315;
-l = zeros(3,8);
-for i = 1:length(a)
-    l(1:2,i) = r*[cosd(a(i));sind(a(i))];
-end
-
-%% thrust decomposition
-gama = 30;
-T = 10 +zeros(1,8);%thrust (N)
-for i = 1:length(T)
-    if mod(i,2) == 0
-        F(1:3,i) = [T(i)*sind(gama)*sind(a(i));-T(i)*sind(gama)*cosd(a(i));T(i)*cosd(gama)];
-    else
-        F(1:3,i) = [-T(i)*sind(gama)*sind(a(i));T(i)*sind(gama)*cosd(a(i));T(i)*cosd(gama)];
-    end
-end
-
-%% moment calculation
-
-for i = 1:length(T)
-    mom(1:3,i) = cross(l(1:3,i),F(1:3,i));
-end
-
-%% linear formation
-for i = 1:length(T)
-    A(1:3,i) = mom(1:3,i)./T(i);
-end
-
-%% Load data from sinulink
-
-Md = out.torque.Data';
-t  = out.torque.time;
-%% allocation 
-f = [1 1 1 1 1 1 1 1];
-for i =1:length(Md)
-    prob = optimproblem;
-    x = optimvar('x',8,1,'LowerBound',0);
-    Aeq = A;
-    beq = Md(1:3,i);
-    cons = Aeq*x == beq;
-    prob.Constraints.cons = cons;
-    prob.Objective = f*x;
-    x1 = solve(prob);
-    u(:,i) = x1.x;
-end
-u_in = [t u'];
-% prob = optimproblem;
-%     x = optimvar('x',8,length(Md),'LowerBound',0);
-%     Aeq = A;
-%     beq = Md;
-%     cons = Aeq*x == beq;
-%     prob.Constraints.cons = cons;
-%     prob.Objective = f*x;
-%     x1 = solve(prob);
-%     u = x1.x;
-=======
 close all;clc;clear
 r = 0.3; %radius(m)
 a = 0:45:315;
@@ -65,8 +6,8 @@ for i = 1:length(a)
     l(1:2,i) = r*[cosd(a(i));sind(a(i))];
 end
 %% Model parameters
-TimeStep = 10^-2;   %Simulation time step(second)
-TimeStop = 10;      %Simulation time (second)
+TimeStep = 10^-3;   %Simulation time step(second)
+TimeStop = 5;      %Simulation time (second)
 %% thrust decomposition
 gam = 30;
 T = 10 +zeros(1,8);%thrust (N)
@@ -89,17 +30,21 @@ for i = 1:length(T)
     A(1:3,i) = Moment_thrust(1:3,i)./T(i);
 end
 
-<<<<<<< Updated upstream
-%% Load data from sinulink
-
-Md = out.torque.Data';
-t  = out.torque.time;
-=======
 %% Simulink
 SimulinkData=sim("lunar_lander_sim_an.slx",TimeStop);
 MomentDemand = SimulinkData.torque.Data'; %moment demand
 
->>>>>>> Stashed changes
+%% Load data from sinulink
+Md = SimulinkData.torque.Data';
+t  = SimulinkData.torque.time;
+LinearTerm = SimulinkData.LinearTerm.Data;
+NonLinearTerm = SimulinkData.NonLinearTerm.Data;
+for i = 1:length(t)
+    LNratio(i,1:3) = LinearTerm(i,1:3)./NonLinearTerm(i,1:3);
+    NLratio(i,1:3) = NonLinearTerm(i,1:3)./LinearTerm(i,1:3);
+end
+
+
 %% allocation 
 f = [1 1 1 1 1 1 1 1];
 for i =1:length(MomentDemand)
@@ -149,7 +94,76 @@ for i = 1:8
     xlabel("time(second)")
     ylabel("Force(N)")
 end
+%% 
+close all
+
+figure(3)
+for i = 1:3
+    subplot(3,1,i)
+    plot(t,LNratio(:,i))
+    xlabel("time(second)")
+    ylabel("Command Torque")
+    switch i
+        case 1
+            title("X")
+        case 2
+            title("y")
+        case 3
+            title("z")
+    end
+    sgtitle("Linear term to NonLinear term Ratio")
+end
+
+
+figure(4)
+for i = 1:3
+    subplot(3,1,i)
+    plot(t,LinearTerm(:,i))
+    xlabel("time(second)")
+    ylabel("Command Torque")
+    switch i
+        case 1
+            title("X")
+        case 2
+            title("y")
+        case 3
+            title("z")
+    end
+    sgtitle("Linear term")
+end
+
+figure(5)
+for i = 1:3
+    subplot(3,1,i)
+    plot(t,NonLinearTerm(:,i))
+    xlabel("time(second)")
+    ylabel("Command Torque")
+    switch i
+        case 1
+            title("X")
+        case 2
+            title("y")
+        case 3
+            title("z")
+    end
+    sgtitle("NonLinear term")
+end
+
+figure(6)
+for i = 1:3
+    subplot(3,1,i)
+    plot(t,NLratio(:,i))
+    xlabel("time(second)")
+    switch i
+        case 1
+            title("X")
+        case 2
+            title("y")
+        case 3
+            title("z")
+    end
+    sgtitle("NonLinear term to Linear term Ratio")
+end
 
 %% pwpf input 
 test = [time_plot' u(1,:)'];
->>>>>>> 10344dcc34c8dd7dc3f9af3de4738584212e0b00
